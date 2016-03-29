af33acb43344672563130a9fc64044ea0b6ffefd
Juraj Kirchheim <back2dos@googlemail.com>
2016-03-29 17:09:32 +0200

    Skip python too. Fail when tests fail.

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -1,12 +1,18 @@
 package;
+
 import haxe.unit.TestRunner;
+
+#if flash
+typedef Sys = flash.system.System;
+#end
 
 class RunTests {
 
   static function main() {
     var t = new TestRunner();
     t.add(new ParserTest());
-    t.run();
+    if (!t.run())
+      Sys.exit(500);
   }
   
 }
