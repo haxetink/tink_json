06f516e9587fe2264abd33601973e65950713b22
Juraj Kirchheim <back2dos@googlemail.com>
2020-04-21 15:56:06 +0200

    Avoid sabotaging autocompletion.

--- src/tink/json/macros/Macro.hx
+++ src/tink/json/macros/Macro.hx
@@ -13,14 +13,14 @@ using StringTools;
 using tink.MacroApi;
 
 class Macro {
-  
+
   static function compact(?prefix:String = '', ?fields:Array<Field>) {
     #if tink_json_compact_code
     if (fields == null)
       fields = Context.getBuildFields();
     for (i in 0...fields.length) {
       var f = fields[i];
-      
+
       if(f.name == 'new') continue;
 
       var meta = {
@@ -32,13 +32,13 @@ class Macro {
         case null: f.meta = [meta];
         case v: v.push(meta);
       }
-    }      
+    }
     return fields;
     #else
     return null;
     #end
   }
-    
+
   static public function nativeName(f:FieldInfo)
     return
       switch f.meta.filter(function (m) return m.name == ':json') {
@@ -46,16 +46,16 @@ class Macro {
         case [{ params: [name] }]: name.getName().sure();
         case [v]: v.pos.error('@:json must have exactly one parameter');
         case v: v[1].pos.error('duplicate @:json metadata not allowed on a single field');
-      }    
-  
-  static function getType(name) 
-    return 
+      }
+
+  static function getType(name)
+    return
       switch Context.getLocalType() {
         case TInst(_.toString() == name => true, [v]):
           v;
         default:
           throw 'assert';
-      }      
+      }
 
   static public function buildParser():Type
     return BuildCache.getType('tink.json.Parser', parser);
@@ -67,24 +67,24 @@ class Macro {
         case [{ params:[{ expr: EConst(CString(v)) }]}]: v;
         case v: c.pos.error('invalid use of @:json');
       }
-  
+
 
   static function parser(ctx:BuildContext):TypeDefinition {
     var name = ctx.name,
         ct = ctx.type.toComplex();
-        
+
     var cl = macro class $name extends tink.json.Parser.BasicParser {
       public function new() super();
-    } 
-    
+    }
+
     function add(t:TypeDefinition)
       cl.fields = cl.fields.concat(t.fields);
-      
+
     var ret = Crawler.crawl(ctx.type, ctx.pos, GenReader.inst);
-    
-    cl.fields = cl.fields.concat(ret.fields);  
-    
-    add(macro class { 
+
+    cl.fields = cl.fields.concat(ret.fields);
+
+    add(macro class {
       public function parse(source):$ct @:pos(ret.expr.pos) {
         this.init(source);
         return ${ret.expr};
@@ -99,23 +99,23 @@ class Macro {
 
   static public function buildWriter():Type
     return BuildCache.getType('tink.json.Writer', writer);
-      
+
   static function writer(ctx:BuildContext):TypeDefinition {
     var name = ctx.name,
         ct = ctx.type.toComplex();
-        
+
     var cl = macro class $name extends tink.json.Writer.BasicWriter {
       public function new() super();
-    } 
-    
+    }
+
     var ret = Crawler.crawl(ctx.type, ctx.pos, GenWriter.inst);
-    
+
     cl.fields = cl.fields.concat(ret.fields);
-    
+
     function add(t:TypeDefinition)
       cl.fields = cl.fields.concat(t.fields);
-    
-    add(macro class { 
+
+    add(macro class {
       public function write(value:$ct):tink.json.Serialized<$ct> {
         this.init();
         ${ret.expr};
@@ -125,38 +125,37 @@ class Macro {
     compact('w', cl.fields);
     return cl;
   }
-  
+
   static public function getRepresentation(t:Type, pos:Position) {
 
     switch t.reduce() {
       case TDynamic(null) | TMono(_) | TAbstract(_.get() => {name: 'Any', pack: []}, _): return None;
-      default: 
+      default:
     }
+
     var ct = t.toComplex({ direct: true });
-    
+
     return
-      switch (macro tink.json.Representation.of((null : $ct)).get()).typeof() {
-        case Success(rep):
-          
-          var rt = rep.toComplex();
-          
-          if (!(macro ((null : tink.json.Representation<$rt>) : $ct)).typeof().isSuccess()) 
-            pos.error('Cannot represent ${t.toString()} in JSON because ${(macro : tink.json.Representation<$rt>).toString()} cannot be converted to ${t.toString()}');
-          
-          Some(rep);
-          
-        default:
-          None;
+      if (Context.unify(t, Context.getType('tink.json.Representation'))) {
+
+        var rep = (macro tink.json.Representation.of((null : $ct)).get()).typeof().sure();
+        var rt = rep.toComplex();
+
+        if (!(macro ((null : tink.json.Representation<$rt>) : $ct)).typeof().isSuccess())
+          pos.error('Cannot represent ${t.toString()} in JSON because ${(macro : tink.json.Representation<$rt>).toString()} cannot be converted to ${t.toString()}');
+
+        Some(rep);
       }
+      else None;
   }
-  
-  static public function shouldSerialize(f:ClassField) 
-    return 
-      !f.meta.has(':transient') 
+
+  static public function shouldSerialize(f:ClassField)
+    return
+      !f.meta.has(':transient')
       && switch f.kind {
         case FVar(AccNever | AccCall, AccNever | AccCall):
           f.meta.has(':isVar');
         case FVar(_, _): true;
         default: false;
-      }  
+      }
 }
